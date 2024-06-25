67318756e45e4876a230393f7bb9bf241703a4b8
Frank MARSHALL <frank@readresolve.io>
2024-06-25 10:59:19 +0200

    Initial commit with business project #1

---
+++ spot-me-business/src/main/java/co/simplon/spot_me_business/SpotMeBusinessApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.spot_me_business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotMeBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotMeBusinessApplication.class, args);
+	}
+
+}
---
+++ spot-me-business/src/test/java/co/simplon/spot_me_business/SpotMeBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.spot_me_business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotMeBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
