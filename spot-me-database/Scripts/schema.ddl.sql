4ee78190dcc927ee0e6e83ef551968a0ce5783a7
Frank MARSHALL <frank@readresolve.io>
2024-06-29 14:53:54 +0200

    Added NOT NULL on natural key cols

--- spot-me-database/Scripts/schema.ddl.sql
+++ spot-me-database/Scripts/schema.ddl.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS t_spots;
 
 CREATE TABLE t_spots (
 	id_spot INT GENERATED ALWAYS AS IDENTITY,
-	spot_name VARCHAR(200),
-	spot_lat DECIMAL(9,6),
-	spot_lng DECIMAL(9,6),
+	spot_name VARCHAR(200) NOT NULL,
+	spot_lat DECIMAL(9,6) NOT NULL,
+	spot_lng DECIMAL(9,6) NOT NULL,
 	spot_desc VARCHAR(2000),
-	spot_img VARCHAR(60),
+	spot_img VARCHAR(41),
 	CONSTRAINT t_spots_pkey PRIMARY KEY (id_spot),
 	CONSTRAINT t_spots_ukey UNIQUE (spot_name, spot_lat, spot_lng)
 );
